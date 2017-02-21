f8e294231a0da934f836ca2dea15fb5f44a9b046
GraciesPadre <ShastasPadre1@gmail.com>
2017-02-21 10:35:43 -0700

    Fixing a typo in build.sh.  Adding dirctories to git ignore.

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ export GOPATH=$(pwd)
 CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo httpProxy
 docker build -t spectralogic/sdk_proxy:latest .
 
-# export GOPATH=$PWD; git install httpProxy
+# export GOPATH=$PWD; go install httpProxy
 # docker run -p 9080:9080 -p 9090:9090 spectralogic/sdk_proxy:latest
 # docker tag 2aacb1b2635b spectralogic/sdk_proxy:latest
 # docker login <creds>
